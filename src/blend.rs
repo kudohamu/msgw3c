752de705181c285405657835cb2ba5883479bcc1
kudohamu <kudohamu91086@gmail.com>
2026-07-31 23:22:22 +0900

    implement non-separatable composite logic

--- src/blend.rs
+++ src/blend.rs
@@ -10,6 +10,7 @@ pub trait Blend: Sized {
     Self::composite_separable(|_cb, cs| cs)(backdrop, self)
   }
 
+  /// https://drafts.csswg.org/compositing-1/#blendingseparable
   fn composite_separable<B: Blend, F>(f: F) -> impl Fn(&B, &Self) -> Self
   where
     F: Fn(f32, f32) -> f32,
@@ -48,11 +49,44 @@ pub trait Blend: Sized {
     }
   }
 
+  /// https://drafts.csswg.org/compositing-1/#blendingnonseparable
   fn composite_non_separable<B: Blend, F>(f: F) -> impl Fn(&B, &Self) -> Self
   where
     F: Fn((f32, f32, f32), (f32, f32, f32)) -> (f32, f32, f32),
   {
-    |backdrop, src| -> Self { Self::from_rgba(0., 0., 0., 1.) }
+    move |backdrop, src| -> Self {
+      let (back_r, back_g, back_b, back_a) = backdrop.non_premultiplied_rgba();
+      let (src_r, src_g, src_b, src_a) = src.non_premultiplied_rgba();
+
+      if src_a == 0. && back_a == 0. {
+        return Self::from_rgba(0., 0., 0., 0.);
+      }
+
+      // Blending: Cr = (1 - αb) x Cs + αb x B(Cb, Cs)
+      // https://drafts.csswg.org/compositing-1/#blending
+      let (b_r, b_g, b_b) = f((back_r, back_g, back_b), (src_r, src_g, src_b));
+      let blended_r = (1. - back_a) * src_r + back_a * b_r;
+      let blended_g = (1. - back_a) * src_g + back_a * b_g;
+      let blended_b = (1. - back_a) * src_b + back_a * b_b;
+
+      // Composite: Co = αs x Fa x Cs + αb x Fb x Cb
+      // https://drafts.csswg.org/compositing-1/#porterduffcompositingoperators
+      let pm_r = src_a * 1. * blended_r + back_a * (1. - src_a) * back_r;
+      let pm_g = src_a * 1. * blended_g + back_a * (1. - src_a) * back_g;
+      let pm_b = src_a * 1. * blended_b + back_a * (1. - src_a) * back_b;
+      // αo = αs x Fa + αb x Fb
+      let a0 = src_a * 1. + back_a * (1. - src_a);
+
+      if a0 == 0. {
+        return Self::from_rgba(0., 0., 0., 0.);
+      }
+
+      let r = (pm_r / a0).clamp(0.0, 1.0);
+      let g = (pm_g / a0).clamp(0.0, 1.0);
+      let b = (pm_b / a0).clamp(0.0, 1.0);
+
+      Self::from_rgba(r, g, b, a0)
+    }
   }
 }
 
